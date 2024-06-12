465a75e5bd06eecd69f0d3d6fc31728f59db8825
ならけいた <146915043+NaraKeita@users.noreply.github.com>
2024-06-12 16:18:25 +0900

    回転　バグ

--- Matrix.h
+++ Matrix.h
@@ -17,8 +17,8 @@ Matrix4x4 MakeIdentity4x4() {
 	return ret;
 }
 
-Matrix4x4 Multiply(const Matrix4x4& m1, const Matrix4x4& m2);
-Matrix4x4 Multiply(const Matrix4x4& m1, const Matrix4x4& m2) {
+Matrix4x4 Multply(const Matrix4x4& m1, const Matrix4x4& m2);
+Matrix4x4 Multply(const Matrix4x4& m1, const Matrix4x4& m2) {
 	Matrix4x4 result = {};
 	for (int i = 0; i < 4; i++) {
 		for (int j = 0; j < 4; j++) {
@@ -54,7 +54,9 @@ Matrix4x4 MakeScaleMatrix(const Vector3& scale) {
 	return ret;
 }
 
-Matrix4x4 MakeRotateXMatrix(float radian) { 
+// 1.X軸回転行列
+Matrix4x4 MakeRotateXMatrix(float radian);
+Matrix4x4 MakeRotateXMatrix(float radian) {
 	Matrix4x4 ret;
 	ret.m[0][0] = 1.0f;
 	ret.m[1][1] = std::cos(radian);
@@ -62,9 +64,11 @@ Matrix4x4 MakeRotateXMatrix(float radian) {
 	ret.m[2][1] = std::sin(-radian);
 	ret.m[2][2] = std::cos(radian);
 	ret.m[3][3] = 1.0f;
+	    
 	return ret;
 }
-
+// 2.Y軸回転行列
+Matrix4x4 MakeRotateYMatrix(float radian);
 Matrix4x4 MakeRotateYMatrix(float radian) {
 	Matrix4x4 ret;
 	ret.m[0][0] = std::cos(radian);
@@ -73,29 +77,38 @@ Matrix4x4 MakeRotateYMatrix(float radian) {
 	ret.m[2][0] = std::sin(radian);
 	ret.m[2][2] = std::cos(radian);
 	ret.m[3][3] = 1.0f;
+		//std::cos(radian), 0.0f, std::sin(-radian), 0.0f, 0.0f, 1.0f, 0.0f, 0.0f, std::sin(radian), 0.0f, std::cos(radian), 0.0f, 0.0f,0.0f, 0.0f, 1.0f;
 	return ret;
 }
 
+
+// 3.Z軸回転行列
+Matrix4x4 MakeRotateZMatrix(float radian);
 Matrix4x4 MakeRotateZMatrix(float radian) {
 	Matrix4x4 ret;
-	ret.m[0][0] = 1.0f;
+	ret.m[0][0] = std::cos(radian);
+	ret.m[0][1] = std::sin(radian);
+	ret.m[1][0] = std::sin(-radian);
 	ret.m[1][1] = std::cos(radian);
-	ret.m[1][2] = std::sin(radian);
-	ret.m[2][1] = std::sin(-radian);
-	ret.m[2][2] = std::cos(radian);
+	ret.m[2][2] = 1.0f;
 	ret.m[3][3] = 1.0f;
+	    /*std::cos(radian), std::sin(radian), 0.0f, 0.0f,
+		std::sin(-radian), std::cos(radian), 0.0f, 0.0f,
+		0.0f, 0.0f, 1.0f, 0.0f, 
+		0.0f, 0.0f, 0.0f, 1.0f,*/
+	
 	return ret;
 }
 
-Matrix4x4 MakeAffineMatrix(const Vector3 scale, const Vector3 rotate, const Vector3 translate);
-Matrix4x4 MakeAffineMatrix(const Vector3 scale, const Vector3 rotate, const Vector3 translate) { 
-	Matrix4x4 rotateMatrixX = MakeRotateXMatrix(rotate.x);
-	Matrix4x4 rotateMatrixY = MakeRotateYMatrix(rotate.y);
-	Matrix4x4 rotateMatrixZ = MakeRotateZMatrix(rotate.z);
-	Matrix4x4 rotateMatrixXYZ = Multiply(rotateMatrixX, Multiply(rotateMatrixY, rotateMatrixZ));
+// 3次元アフィン変換行列
+Matrix4x4 MakeAffineMatrix(const Vector3& scale, const Vector3& rotate, const Vector3& translate);
+Matrix4x4 MakeAffineMatrix(const Vector3& scale, const Vector3& rotate, const Vector3& translate) {
+	Matrix4x4 rotateXMatrix = MakeRotateXMatrix(rotate.x);
+	Matrix4x4 rotateYMatrix = MakeRotateYMatrix(rotate.y);
+	Matrix4x4 rotateZMatrix = MakeRotateZMatrix(rotate.z);
+	Matrix4x4 rotateXYZMatrix = Multply(rotateXMatrix, Multply(rotateYMatrix, rotateZMatrix));
 	Matrix4x4 scaleMatrix = MakeScaleMatrix(scale);
 	Matrix4x4 translateMatrix = MakeTranslateMatrix(translate);
-	Matrix4x4 ret = Multiply(scaleMatrix, Multiply(rotateMatrixXYZ, translateMatrix));
+	Matrix4x4 ret = Multply(scaleMatrix, Multply(rotateXYZMatrix, translateMatrix));
 	return ret;
 }
-
