c68539798c3411eb634eef4ae923d47ab640a0d0
ならけいた <146915043+NaraKeita@users.noreply.github.com>
2024-11-11 16:38:37 +0900

    おかしい

--- WinApp.cpp
+++ WinApp.cpp
@@ -2,20 +2,20 @@
 
 void WinApp::Initialize() {
 
-	HWND hwnd = CreateWindow(
-		wc.lpszClassName, 
-		L"CG2",
-		WS_OVERLAPPEDWINDOW,
-		CW_USEDEFAULT,
-		CW_USEDEFAULT,
-		wrc.right - wrc.left,
-		wrc.bottom - wrc.top,
-		nullptr, 
-		nullptr,
-		wc.hInstance,
-		nullptr
-	);
-	ShowWindow(hwnd, SW_SHOW);
+	//HWND hwnd = CreateWindow(
+	//	wc.lpszClassName, 
+	//	L"CG2",
+	//	WS_OVERLAPPEDWINDOW,
+	//	CW_USEDEFAULT,
+	//	CW_USEDEFAULT,
+	//	wrc.right - wrc.left,
+	//	wrc.bottom - wrc.top,
+	//	nullptr, 
+	//	nullptr,
+	//	wc.hInstance,
+	//	nullptr
+	//);
+	//ShowWindow(hwnd, SW_SHOW);
 }
 
 void WinApp::Update() {
