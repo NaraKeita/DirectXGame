0b316e4cace367d65cf4a05f740c42be97fd63ea
ならけいた <146915043+NaraKeita@users.noreply.github.com>
2024-11-29 12:29:45 +0900

    途中

--- DirectXCommon.cpp
+++ DirectXCommon.cpp
@@ -7,6 +7,20 @@
 using namespace Microsoft::WRL;
 using namespace Logger;
 
+std::wstring ConvertString(const std::string& str) {
+	if (str.empty()) {
+		return std::wstring();
+	}
+
+	auto sizeNeeded = MultiByteToWideChar(CP_UTF8, 0, reinterpret_cast<const char*>(&str[0]), static_cast<int>(str.size()), NULL, 0);
+	if (sizeNeeded == 0) {
+		return std::wstring();
+	}
+	std::wstring result(sizeNeeded, 0);
+	MultiByteToWideChar(CP_UTF8, 0, reinterpret_cast<const char*>(&str[0]), static_cast<int>(str.size()), &result[0], sizeNeeded);
+	return result;
+}
+
 void DirectXCommon::Initialize() {
 	// NULL検出
 	assert(winApp);
